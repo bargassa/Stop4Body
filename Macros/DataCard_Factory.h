923bc2a061f02e1024ce5a3e79605888e0e37b06
Pedrame Bargassa <bargassa@cern.ch>
2017-06-22 15:37:18 +0100

    22 June

---
+++ Macros/DataCard_Factory.h
@@ -0,0 +1,47 @@
+
+void DataCard_Factory();
+void GetYield(double ,     
+	      string ,       
+	      string ,       
+	      TCut ,    
+	      TCut ,  
+	      TCut ,  
+	      double& ,   
+	      double& ,   
+	      double& ,   
+	      double& ,  
+	      double& ,  
+	      double& 
+	      );
+void GetSysISR(double ,     
+	       string ,       
+	       string ,       
+	       TCut ,    
+	       double& ,   
+	       double& ,  
+	       double& ,  
+	       double& ,   
+	       double& ,  
+	       double& ,  
+	       double& ,  
+	       double& ,  
+	       double& ,   
+	       double& ,  
+	       double& ,   
+	       double& ,  
+	       double& ,  
+	       double& 
+	       );
+void GetSysIDISO(double ,     
+		 string ,       
+		 string ,       
+		 TCut ,  
+		 double& ,   
+		 double& ,   
+		 double& ,  
+		 double& 
+		 );
+double eff(double , double); 
+double uneff(double , double);
+double Max(double , double);
+double AbsV(double); 
